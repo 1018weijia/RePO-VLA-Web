28e23b616eacb1ab76b373c4674b7bbc3d6e9196
lfwj <2206649499@qq.com>
2026-05-18 23:17:22 +0800

    add_real_world_demo

--- src/mock/data.ts
+++ src/mock/data.ts
@@ -227,6 +227,30 @@ export const demoSections = [
       // },
     ],
   },
+
+  {
+  title: 'Real-World Robot Demos',
+  desc: 'Real robot executions under practical disturbances and long-horizon tasks.',
+  videos: [
+    {
+      title: 'Cook Vegetable',
+      src: asset('Demo/Real_world/cook_vegetable.mp4'),
+    },
+    {
+      title: 'Pick Fruits',
+      src: asset('Demo/Real_world/pick_fruits.mp4'),
+    },
+    {
+      title: 'Pour Water with Human Perturbation',
+      src: asset('Demo/Real_world/pour_water_with_perturbation.mp4'),
+    },
+    {
+      title: 'Pour Water with Human Perturbation2',
+      src: asset('Demo/Real_world/pour_water_with_perturbation2.mp4'),
+    },
+  ],
+}
+
 ]
 
 export const resources = [
